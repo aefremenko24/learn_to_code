efc6c10ee666e9adf44e7c19e89f26a27fdd1376
Arthur Efremenko <aefremenko24@gmail.com>
2024-11-16 13:27:16 -0500

    initial commit

---
+++ learn_to_code/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  learn_to_code
+//
+//  Created by Arthur Efremenko on 11/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ learn_to_code/learn_to_codeApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_to_codeApp.swift
+//  learn_to_code
+//
+//  Created by Arthur Efremenko on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct learn_to_codeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
